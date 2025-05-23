c951604bc22a3abe39437688b029780d0fe6d9bf
gchinaul <gabriel.chinault@outlook.com>
2025-05-23 17:20:01 +0200

    Add files via upload

--- exam03/get_next_line.c
+++ exam03/get_next_line.c
@@ -1,50 +1,49 @@
+
 #include "get_next_line.h"
 
-char	*ft_strdup(char *src)
+char *ft_strdup(char *src)
 {
-	char	*dest;
-	int		i;
+    char *dest;
+    int i = 0;
 
-	i = 0;
-	while (src[i])
-		i++;
-	dest = (char *)malloc(sizeof(char) * (i + 1));
-	i = 0;
-	while (src[i])
-	{
-	   dest[i] = src[i];
-	   i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+    while(src[i])
+        i++;
+    dest = (char *)malloc(sizeof(char) * (i + 1));
+    i = 0;
+    while (src[i])
+    {
+        dest[i] = src[i];
+        i++;
+    }
+    dest[i] = '\0';
+    return (dest);
 }
 
-char	*get_next_line(int fd)
+char    *get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE];
-	char		line[70000];
-	static int	buffer_read;
-	static int 	buffer_pos;
-	int			i;
+    static char buff[BUFFER_SIZE];
+    char li[70000];
+    static int b_r;
+    static int b_p;
+    int i = 0;
 
-	i = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-	while (1)
-	{
-		if (buffer_pos >= buffer_read)
-		{
-			buffer_read = read(fd, buffer, BUFFER_SIZE);
-			buffer_pos = 0;
-			if (buffer_read <= 0)
-				break ;
-		}
-		line[i++] = buffer[buffer_pos++];
-		if (buffer[buffer_pos] == '\n')
-			break ;
-	}
-	line[i] = '\0';
-	if (i == 0)
-		return (NULL);
-	return (ft_strdup(line));
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    while (1)
+    {
+        if (b_p >= b_r)
+        {
+            b_r = read(fd, buff, BUFFER_SIZE);
+            b_p = 0;
+            if (b_r <= 0)
+                break ;
+        }
+        li[i++] = buff[b_p++];
+        if (buff[b_p] == '\n')
+            break ;
+    }
+    li[i] = '\0';
+    if (i == 0)
+        return (NULL);
+    return(ft_strdup(li));
 }
--- exam03/get_next_line.h
+++ exam03/get_next_line.h
@@ -1,15 +1,17 @@
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>	
-# include <stdio.h>
-# include <fcntl.h>
+
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
+#   define BUFFER_SIZE 42
 # endif
 
-char	*get_next_line(int fd);
+char    *get_next_line(int fd);
 
 #endif
